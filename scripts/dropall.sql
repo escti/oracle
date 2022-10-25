6b68bd36472a5fecbf6f00d7bb4ec971187e7344
escti <114307231+escti@users.noreply.github.com>
2022-10-24 23:59:44 -0300

    Update dropall.sql
    -- -----------------------------------------------------------------------------------
    -- File Name    : dropall.sql
    -- Author       : Thiago Escodino (thiago@escti.net)
    -- Description  : Gera comandos para apagar objetos de um schema.
    -- Call Syntax  : @dropall.sql
    -- Last Modified: 07/07/2022
    -- -----------------------------------------------------------------------------------

--- scripts/dropall.sql
+++ scripts/dropall.sql
@@ -1,27 +1,30 @@
-@env_s
-
+-- -----------------------------------------------------------------------------------
+-- File Name    : dropall.sql
+-- Author       : Thiago Escodino (thiago@escti.net)
+-- Description  : Gera comandos para apagar objetos de um schema.
+-- Call Syntax  : @dropall.sql
+-- Last Modified: 07/07/2022
+-- -----------------------------------------------------------------------------------
 set heading off
 set verify off
 set feedback off
 
-prompt @dropall <owner>
-
 prompt ALTER SESSION SET RECYCLEBIN=OFF;;
 
-select 
-	case 
-		when object_type = 'TABLE' then 'drop ' || object_type || ' "' || owner || '"."' || object_name ||'" cascade constraints'
-		when object_type in ('OPERATOR', 'TYPE') then 'drop ' || object_type || ' "' || owner || '"."' || object_name ||'" force'
-		else 'drop ' || object_type || ' "' || owner || '"."' || object_name || '"'
-	end || ';'
-from dba_objects
-where owner = upper('&&1')
-and object_type not in ('INDEX', 'LOB', 'PACKAGE BODY', 'TRIGGER')
-order by object_type;
+SELECT CASE
+           WHEN object_type = 'TABLE' THEN 'drop ' || object_type || ' "' || OWNER || '"."' || object_name ||'" cascade constraints'
+           WHEN object_type in ('OPERATOR',
+                                'TYPE') THEN 'drop ' || object_type || ' "' || OWNER || '"."' || object_name ||'" force'
+           ELSE 'drop ' || object_type || ' "' || OWNER || '"."' || object_name || '"'
+       END || ';'
+FROM dba_objects
+WHERE OWNER = upper('&&1')
+  AND object_type not in ('INDEX',
+                          'LOB',
+                          'PACKAGE BODY',
+                          'TRIGGER')
+ORDER BY object_type;
 
 prompt ALTER SESSION SET RECYCLEBIN=ON;;
-spo off
 
 undef 1
-
-@env_l
